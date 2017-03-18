31d974338452cbff55c936e2e802ec5b6c233b17
Iurii Nikolaev <jusnikolaev@gmail.com>
2017-03-18 16:02:53 +0300

    Bug fix

--- space_bro_bot.py
+++ space_bro_bot.py
@@ -16,42 +16,47 @@ PLANETS = {
 
 #Ответ на текстовое сообщение пользователя
 def info(bot, update):
-    info_text = "\t Commands: \n /constellation NAME - planet constellation , \n /position NAME - planet position in current moment"
-    bot.sendMessage(update.message.chat_id, text = info_text)
+    info_text = """\t Commands: 
+ /constellation NAME - planet constellation , 
+ /position NAME - planet position in current moment"""
+    bot.sendMessage(update.message.chat_id, text=info_text)
 
 #Функция вычисления созвездия
 def planet_constellation(planet):
     date = datetime.datetime.now()
     curr_date = date.strftime('%Y/%m/%d')
-    planet_constll = PLANETS.get(planet, "Sry...I dont know")
-    if isinstance(planet_constll, str):
-        return planet_constll
+    #if isinstance(planet_constll, str):
+    if planet not in PLANETS:
+        return "Sry...I dont know"
     else:
+        planet_constll = PLANETS[planet]
         return constellation(planet_constll(str(curr_date)))
-    
-   
+
 #Функция вычисления положения планеты
 def planet_position(planet):
     date = datetime.datetime.now()
     curr_date = date.strftime('%Y/%m/%d')
-    planet_ctor = PLANETS.get(planet, "Sry...I dont know")
-    planet_pos = planet_ctor()
-    if isinstance(planet_ctor, str):
-        return planet_pos
-    else:
+    if planet in PLANETS:
+        planet_ctor = PLANETS[planet]
+        planet_pos = planet_ctor()
         planet_pos.compute(curr_date)
         return planet_pos
+    else:
+        return None
 
 def constellation_bot(bot, update, args):
-    planet = str(args[0]).lower()
+    planet = str(args[0] if args else '').lower()
     planet_coord = planet_constellation(planet)
     bot.sendMessage(update.message.chat_id, text = str(planet_coord))
 
 def planet_position_bot(bot, update, args):
-    planet = str(args[0]).lower()
-    right_ascension = str(planet_position(planet).a_ra)
-    declination = str(planet_position(planet).a_dec)
-    bot.sendMessage(update.message.chat_id, text = declination + ' , ' + right_ascension)
+    planet = str(args[0] if args else '').lower()
+    planet_pos = planet_position(planet)
+    if not planet_pos:
+         bot.sendMessage(update.message.chat_id, text = 'Sry... I dont know')
+    else:
+        right_ascension, declination = str(planet_pos.a_ra), str(planet_pos.a_dec)
+        bot.sendMessage(update.message.chat_id, text = declination + ' , ' + right_ascension)
     
 
 #Главная функция 
